1a346fb9527554d6359154aa5ab07b8303610090
orange811024 <orange811024@gmail.com>
2024-10-18 16:35:04 +0800

    lesson4_5.py

--- lesson4/lesson4_5.py
+++ lesson4/lesson4_5.py
@@ -4,21 +4,21 @@ from tkinter import ttk
 class Window(ThemedTk):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.title('使用ttk的套件')
+        self.title('使用ttk的套件，這是title')
         style = ttk.Style(self)
         topFrame = ttk.Frame(self,width=300,height=100,borderwidth=3,relief='groove')
         topFrame.pack(padx=10,pady=(10,0),expand=True,fill='x')
         btn1 = ttk.Button(topFrame,text='按鈕1')
-        btn1.pack(side='left')
+        btn1.pack(side='left',expand=True,fill='x',padx=10)
         btn2 = ttk.Button(topFrame,text='按鈕2')
-        btn2.pack(side='left')
+        btn2.pack(side='left',expand=True,fill='x',padx=10)
         btn3 = ttk.Button(topFrame,text='按鈕3')
-        btn3.pack(side='left')
+        btn3.pack(side='left',expand=True,fill='x',padx=10)
         bottomFrame = ttk.Frame(self,width=500,height=300,borderwidth=3,relief='groove')
         bottomFrame.pack(padx=10,pady=10)
 
 def main():
-    window = Window(theme='arc')
+    window = Window(theme='radiance')
     window.mainloop()
 
 if __name__ == '__main__':
