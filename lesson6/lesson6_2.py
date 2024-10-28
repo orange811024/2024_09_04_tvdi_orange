b6a6532281ad7b046fba0e40392fe61c08911417
orange811024 <orange811024@gmail.com>
2024-10-28 13:37:55 +0800

    lesson6_2.py

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -1,3 +1,42 @@
 import datasource
-sitenames = datasource.get_sitenames()
-print(sitenames)
+from tkinter import ttk
+import tkinter as tk
+from ttkthemes import ThemedTk
+from tkinter.messagebox import showinfo
+
+class Window(ThemedTk):
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title('登入')
+        #==============style===============
+        style = ttk.Style(self)
+        style.configure('TopFrame.TLabel',font=('Helvetica',20))
+        #============end style===============
+        
+        #==============top Frame===============
+        topFrame = ttk.Frame(self)
+        ttk.Label(topFrame,text='空氣品質指標(AQI)(歷史資料)',style='TopFrame.TLabel').pack()
+        topFrame.pack(padx=20,pady=20)
+        #==============end topFrame===============
+
+        #==============bottomFrame===============
+        bottomFrame = ttk.Frame(self)
+        sitenames = datasource.get_sitenames()
+        self.selected_site = tk.StringVar()
+        sitenames_cb = ttk.Combobox(bottomFrame, textvariable=self.selected_site,values=sitenames)
+        sitenames_cb.set('請選擇站點')
+        sitenames_cb.pack()
+        bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
+        #==============end bottomFrame===============
+    
+    def agreement_changed(self):
+        showinfo(
+            title='Agreement',
+            message= self.agreement.get())
+
+def main():
+    window = Window(theme="arc")
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
